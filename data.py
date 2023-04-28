03239d8291e15088db0d729b59a7caa077880d26
PogoSpin <daniel.sp.nyborg@gmail.com>
2023-04-28 23:49:49 +0400

    Created new data file format and system.

---
+++ data.py
@@ -0,0 +1,7 @@
+# This file will contain both your OpenAI key and the positions of every mouse movement.
+# You can manually paste your OpenAI key below but this is prompted in the program.
+
+key = '' # you can obtain the api key through: https://platform.openai.com/account/api-keys
+
+positions = []   # This starts blank but it is generated once you set it up through the program. It is the positions of the bottons on google slides.
+                 # These positions are saved in a list format like this [addSlideButtonPos, titleBoxPos, textBoxPos]
