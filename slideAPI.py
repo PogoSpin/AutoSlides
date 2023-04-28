8b79d52c4f29ee277acf962376e2aca56c877429
PogoSpin <daniel.sp.nyborg@gmail.com>
2023-04-28 23:52:39 +0400

    Made it object orriented and changed some names

--- slideAPI.py
+++ slideAPI.py
@@ -2,7 +2,7 @@ import pyautogui as pa
 
 class Slides:
     mouseMoveSpeed = 0.5
-    def __init__(self, titlePos, textPos, newSlidePos):
+    def __init__(self, newSlidePos, titlePos, textPos):
         self.title = titlePos                   # (x, y) pos of the title box
         self.text = textPos                     # (x, y) pos of the main text box
         self.newSlide = newSlidePos             # (x, y) pos of the new slide button
