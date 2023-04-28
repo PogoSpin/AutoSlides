bbf220df61e20f23f243e8948a3efcaca2bda358
PogoSpin <daniel.sp.nyborg@gmail.com>
2023-04-28 23:58:17 +0400

    Deleted old files

--- info.py
+++
@@ -1,6 +0,0 @@
-# This file will contain both your OpenAI key and the positions of every mouse movement.
-# The only thing you need to do manually is paste your OpenAI key below:
-
-key = '' # you can obtain the api key through: https://platform.openai.com/account/api-keys
-
-positions = None   # This starts blank but it is generated once you set it up through the program
