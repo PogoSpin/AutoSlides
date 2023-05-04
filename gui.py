fa2084a9490d4ca08be965f8e6c36ac384cf7f09
PogoSpin <daniel.sp.nyborg@gmail.com>
2023-05-04 11:30:53 +0400

    Made very simple gui

--- gui.py
+++ gui.py
@@ -1,13 +1,28 @@
-import tkinter as tk
-from customtkinter import *
- 
-class App(CTk):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
- 
-        self.main_frame = CTkFrame(self)
-        self.main_frame.pack(expand=True, fill=tk.BOTH)
- 
-app = App()
-app.geometry('1920x1080')
-app.mainloop()
+import customtkinter as ctk
+
+# window setup
+ctk.set_appearance_mode('dark')
+ctk.set_default_color_theme('dark-blue')
+
+window = ctk.CTk()
+window.title('Auto Slides')
+window.geometry('500x300')
+
+window.grid_columnconfigure(0, weight=1)       # makes everything centered??    https://customtkinter.tomschimansky.com/tutorial/
+
+# widgets
+label = ctk.CTkLabel(window, text = 'Auto Slides', font = ('arial', 40))
+label.grid(pady = 20)
+
+topic = ctk.CTkEntry(window, placeholder_text = 'Presentation Topic')
+topic.grid(pady = 15, padx = 60, sticky = 'ew')
+
+topic = ctk.CTkEntry(window, placeholder_text = 'Amount of Slides')
+topic.grid(pady = 0, padx = 60, sticky = 'ew')
+
+button = ctk.CTkButton(window, text = 'Generate Slides')
+button.grid(pady = 40, padx = 60, sticky = 'ew')
+
+
+# run
+window.mainloop()
