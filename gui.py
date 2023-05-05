6ffa80e84d4a522287dc47c829d8ee16bfefa658
PogoSpin <daniel.sp.nyborg@gmail.com>
2023-05-06 01:32:58 +0400

    Made the padding adaptable

--- gui.py
+++ gui.py
@@ -4,24 +4,26 @@ import customtkinter as ctk
 ctk.set_appearance_mode('dark')
 ctk.set_default_color_theme('dark-blue')
 
+xSize = 500
+
 window = ctk.CTk()
 window.title('Auto Slides')
-window.geometry('500x300')
+window.geometry(f'{xSize}x300')
 
 window.grid_columnconfigure(0, weight=1)       # makes everything centered??    https://customtkinter.tomschimansky.com/tutorial/
 
 # widgets
 label = ctk.CTkLabel(window, text = 'Auto Slides', font = ('arial', 40))
-label.grid(pady = 20)
+label.grid(pady = 30)
 
 topic = ctk.CTkEntry(window, placeholder_text = 'Presentation Topic')
-topic.grid(pady = 15, padx = 60, sticky = 'ew')
+topic.grid(pady = 15, padx = xSize/8.333, sticky = 'ew')
 
 topic = ctk.CTkEntry(window, placeholder_text = 'Amount of Slides')
-topic.grid(pady = 0, padx = 60, sticky = 'ew')
+topic.grid(pady = 0, padx = xSize/8.333, sticky = 'ew')
 
 button = ctk.CTkButton(window, text = 'Generate Slides')
-button.grid(pady = 40, padx = 60, sticky = 'ew')
+button.grid(pady = 40, padx = xSize/8.333, sticky = 'ew')
 
 
 # run
