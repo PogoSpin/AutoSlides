a0556a4509aefc20d25273bb889f41cc5a910593
PogoSpin <daniel.sp.nyborg@gmail.com>
2023-05-11 10:26:36 +0400

    Restructured and added api entry and gen slides Restructured to a class system Added api key entry and help button with openai link Added generated slides scrollable frame Changed window size Moved elements

--- gui.py
+++ gui.py
@@ -1,30 +1,94 @@
 import customtkinter as ctk
+import webbrowser
 
-# window setup
-ctk.set_appearance_mode('dark')
-ctk.set_default_color_theme('dark-blue')
+class ScrollableEntryFrame(ctk.CTkScrollableFrame):
+    def __init__(self, master, item_list, command=None, **kwargs):
+        super().__init__(master, **kwargs)
 
-xSize = 500
+        self.command = command
+        self.checkbox_list = []
+        for i, item in enumerate(item_list):
+            self.add_item(item)
 
-window = ctk.CTk()
-window.title('Auto Slides')
-window.geometry(f'{xSize}x300')
+    def add_item(self, item):
+        checkbox = ctk.CTkCheckBox(self, text=item)
+        if self.command is not None:
+            checkbox.configure(command=self.command)
+        checkbox.grid(row=len(self.checkbox_list), column=0, pady=(0, 10))
+        self.checkbox_list.append(checkbox)
 
-window.grid_columnconfigure(0, weight=1)       # makes everything centered??    https://customtkinter.tomschimansky.com/tutorial/
+    def remove_item(self, item):
+        for checkbox in self.checkbox_list:
+            if item == checkbox.cget("text"):
+                checkbox.destroy()
+                self.checkbox_list.remove(checkbox)
+                return
 
-# widgets
-label = ctk.CTkLabel(window, text = 'Auto Slides', font = ('arial', 40))
-label.grid(pady = 30)
+    def get_checked_items(self):
+        return [checkbox.cget("text") for checkbox in self.checkbox_list if checkbox.get() == 1]
 
-topic = ctk.CTkEntry(window, placeholder_text = 'Presentation Topic')
-topic.grid(pady = 15, padx = xSize/8.333, sticky = 'ew')
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        # app setup
 
-topic = ctk.CTkEntry(window, placeholder_text = 'Amount of Slides')
-topic.grid(pady = 0, padx = xSize/8.333, sticky = 'ew')
+        self.size = (1000, 700)
 
-button = ctk.CTkButton(window, text = 'Generate Slides')
-button.grid(pady = 40, padx = xSize/8.333, sticky = 'ew')
+        self.title('Auto Slides')
+        self.geometry(f'{self.size[0]}x{self.size[1]}')
+
+        self.resizable(False, False)
+        self.grid_columnconfigure(0, weight=1)       # makes everything centered??    https://customtkinter.tomschimansky.com/tutorial/
+
+        def centerObj(width):
+            return (self.size[0]/2) - (width/2)
+
+
+        def helpButton():
+            webbrowser.open('https://platform.openai.com/account/api-keys')
+
+
+        # widgets
+
+        # Main center part
+        title = ctk.CTkLabel(self, text = 'Auto Slides', font = ('arial', 40))
+        title.grid(pady = 30)
+
+        topic = ctk.CTkEntry(self, placeholder_text = 'Presentation Topic', width = 350)
+        topic.place(x = centerObj(350), y = 150)
+
+        numOfSlides = ctk.CTkEntry(self, placeholder_text = 'Amount of Slides', width = 350)
+        numOfSlides.place(x = centerObj(350), y = 190)
+
+        genSlides = ctk.CTkButton(self, text = 'Generate Slides', width = 350)
+        genSlides.place(x = centerObj(350), y = 250)
+
+
+
+        # Left info section
+        info = ctk.CTkLabel(self, text = 'Info', font = ('arial', 30))
+        info.place(x = 125, y = 35)
+
+        apiKey = ctk.CTkEntry(self, placeholder_text = 'API Key', width = 180)
+        apiKey.place(x = 50, y = 150)
+
+        apiHelp = ctk.CTkButton(self, text = 'Help', width = 50, command = helpButton)
+        apiHelp.place(x = 235, y = 150)
+
+
+
+        # Right Side
+
+        slidesFrame = ctk.CTkScrollableFrame(self, width = 250, height = 300, label_text = 'Generated Slides')
+        slidesFrame.place(x = 700, y = 150)
+
+
 
 
 # run
-window.mainloop()
+
+if __name__ == '__main__':
+    ctk.set_appearance_mode('dark')
+    ctk.set_default_color_theme('dark-blue')
+    app = App()
+    app.mainloop()
