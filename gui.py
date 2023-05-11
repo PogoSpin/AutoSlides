8395a63fffd91a49819b38ca4be554f13b22aa6f
PogoSpin <daniel.sp.nyborg@gmail.com>
2023-05-11 20:41:23 +0400

    Added entries to the frame WIP

--- gui.py
+++ gui.py
@@ -6,26 +6,26 @@ class ScrollableEntryFrame(ctk.CTkScrollableFrame):
         super().__init__(master, **kwargs)
 
         self.command = command
-        self.checkbox_list = []
+        self.slideList = []
         for i, item in enumerate(item_list):
             self.add_item(item)
 
     def add_item(self, item):
-        checkbox = ctk.CTkCheckBox(self, text=item)
+        slide = ctk.CTkEntry(self, placeholder_text = item)           # add generated slides
         if self.command is not None:
-            checkbox.configure(command=self.command)
-        checkbox.grid(row=len(self.checkbox_list), column=0, pady=(0, 10))
-        self.checkbox_list.append(checkbox)
+            slide.configure(command=self.command)
+        slide.grid(row=len(self.slideList), column=0, pady=(0, 10))
+        self.slideList.append(slide)
 
     def remove_item(self, item):
-        for checkbox in self.checkbox_list:
-            if item == checkbox.cget("text"):
-                checkbox.destroy()
-                self.checkbox_list.remove(checkbox)
+        for slide in self.slideList:
+            if item == slide.cget('text'):
+                slide.destroy()
+                self.slideList.remove(slide)
                 return
 
     def get_checked_items(self):
-        return [checkbox.cget("text") for checkbox in self.checkbox_list if checkbox.get() == 1]
+        return [slide.cget('text') for slide in self.slideList if slide.get() == 1]
 
 class App(ctk.CTk):
     def __init__(self):
@@ -51,36 +51,37 @@ class App(ctk.CTk):
         # widgets
 
         # Main center part
-        title = ctk.CTkLabel(self, text = 'Auto Slides', font = ('arial', 40))
-        title.grid(pady = 30)
+        self.title = ctk.CTkLabel(self, text = 'Auto Slides', font = ('arial', 40))
+        self.title.grid(pady = 30)
 
-        topic = ctk.CTkEntry(self, placeholder_text = 'Presentation Topic', width = 350)
-        topic.place(x = centerObj(350), y = 150)
+        self.topic = ctk.CTkEntry(self, placeholder_text = 'Presentation Topic', width = 350)
+        self.topic.place(x = centerObj(350), y = 150)
 
-        numOfSlides = ctk.CTkEntry(self, placeholder_text = 'Amount of Slides', width = 350)
-        numOfSlides.place(x = centerObj(350), y = 190)
+        self.numOfSlides = ctk.CTkEntry(self, placeholder_text = 'Amount of Slides', width = 350)
+        self.numOfSlides.place(x = centerObj(350), y = 190)
 
-        genSlides = ctk.CTkButton(self, text = 'Generate Slides', width = 350)
-        genSlides.place(x = centerObj(350), y = 250)
+        self.genSlides = ctk.CTkButton(self, text = 'Generate Slides', width = 350)
+        self.genSlides.place(x = centerObj(350), y = 250)
 
 
 
         # Left info section
-        info = ctk.CTkLabel(self, text = 'Info', font = ('arial', 30))
-        info.place(x = 125, y = 35)
+        self.info = ctk.CTkLabel(self, text = 'Info', font = ('arial', 30))
+        self.info.place(x = 125, y = 35)
 
-        apiKey = ctk.CTkEntry(self, placeholder_text = 'API Key', width = 180)
-        apiKey.place(x = 50, y = 150)
+        self.apiKey = ctk.CTkEntry(self, placeholder_text = 'API Key', width = 180)
+        self.apiKey.place(x = 50, y = 150)
 
-        apiHelp = ctk.CTkButton(self, text = 'Help', width = 50, command = helpButton)
-        apiHelp.place(x = 235, y = 150)
+        self.apiHelp = ctk.CTkButton(self, text = 'Help', width = 50, command = helpButton)
+        self.apiHelp.place(x = 235, y = 150)
 
 
 
         # Right Side
 
-        slidesFrame = ctk.CTkScrollableFrame(self, width = 250, height = 300, label_text = 'Generated Slides')
-        slidesFrame.place(x = 700, y = 150)
+        # self.slidesFrame = ctk.CTkScrollableFrame(self, width = 250, height = 300, label_text = 'Generated Slides')
+        self.slidesFrame = ScrollableEntryFrame(self, label_text = 'Generated Slides', width = 250, height = 300, item_list=[f'item {i}' for i in range(50)])
+        self.slidesFrame.place(x = 700, y = 150)
 
 
 
